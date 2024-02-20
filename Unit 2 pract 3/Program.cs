5cfe527362d6cd47bca9a58d90dccf6f7a40c437
drp_3 <drp_3@DESKTOP-T87EQCP>
2024-02-19 23:02:31 -0600

    Agregar archivos de proyecto.

---
+++ Unit 2 pract 3/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Animal
+{
+    public string nombre { get; set; }
+
+    public void Comer()
+    {
+        Console.WriteLine(nombre + " está comiendo.");
+    }
+}
+
+public class Perro : Animal
+{
+    public void Ladrar()
+    {
+        Console.WriteLine(nombre + " está ladrando.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Perro miPerro = new Perro();
+        miPerro.nombre = "Fido";
+        miPerro.Comer();
+        miPerro.Ladrar();
+    }
+}
